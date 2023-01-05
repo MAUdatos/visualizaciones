472272dc645e3afa00b06a9f4cc0fd6ee99b2907
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-04 22:57:14 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -37,7 +37,7 @@ st.caption('Sistematización y Mapeo. Prototipo Web App  1.0')
 tab1, tab2 = st.tabs(["Objetivo General", "Objetivos Específicos"])
 
 with tab1:
-   st.markdown("""- Desarrollar una red de cooperación mutua que fomente, reivindique y defienda el oficio de la agroecología en pro de la soberanía alimentaria (MAU 3/12/2022)       """)
+   st.markdown("""- Desarrollar una red de cooperación mutua que fomente, reivindique y defienda el oficio de la agroecología en pro de la soberanía alimentaria""") #(MAU 3/12/2022)
    st.markdown(""" """)
    st.markdown(""" """)
    st.markdown(""" """)    #varias lineas vacias para evitar un salto de pagina al cambiar entre Objetivo General y Objetivos Especificos
