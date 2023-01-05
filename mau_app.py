4210ddeb98447abbc1e8d7cc244ff7b6fbfc02ef
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-05 01:53:02 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -75,7 +75,7 @@ def dms2dd(s):
 #df_geo['Latitude']  = df_geo['Latitude'].apply(dms2dd)
 #df_geo['Longitude'] = df_geo['Longitude'].apply(dms2dd)
 #st.write(df_geo)
-#df = pd.DataFrame(df_geo,columns=['Latitude', 'Longitude'])
+#df = pd.DataFrame(df_geo,columns=['Latitude','Longitude'])
 #st.map(df)
 
 st.caption('Fuente: Formularios de participación en 1er y 2do Encuentro MAU 2022')
