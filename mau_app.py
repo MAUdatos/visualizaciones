5060e6fd817106042cad892b3e52f8f244cdee6a
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-05 01:15:19 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -68,12 +68,13 @@ def dms2dd(s):
 
 df_geo = df_bbdd[['Latitud','Longitud']].dropna(how = 'all')
 df_geo = df_geo.replace(['No info'], '').dropna(how = 'all')
+df_geo = df_geo.replace(['No Info'], '').dropna(how = 'all')
 st.write(df_geo)
 #df_geo['Latitude']  = df_geo['Latitude'].apply(dms2dd)
 #df_geo['Longitude'] = df_geo['Longitude'].apply(dms2dd)
 
-#df = pd.DataFrame(df_geo,columns=['Latitude', 'Longitude'])
-#st.map(df)
+df = pd.DataFrame(df_geo,columns=['Latitude', 'Longitude'])
+st.map(df)
 
 st.caption('Fuente: Formularios de participación en 1er y 2do Encuentro MAU 2022')
 st.markdown("""---""")
