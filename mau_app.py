39bc1ebf154a70e400d9fe9f8da93a3c49b2731b
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-04 22:57:48 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -78,7 +78,7 @@ st.markdown('1er Encuentro:   11/2022')
 st.markdown('2do Encuentro:   12/2022')
 st.subheader('Expectativas')
 st.markdown(
-'Análisis de respuestas a preguntas: /n"
+'Análisis de respuestas a preguntas: /n'
 '*"¿Cuáles serían los objetivos de esta articulación [Movimiento]?"* (1er Encuentro) /n'
 '*"¿Qué esperas de una articulación entre huertas urbanas? Tus ideas nos pueden ayudar delinear el programa de futuros encuentros (2do encuentro).*"'
 )
