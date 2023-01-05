d990bbbde9dcc04dadf9449654c5bdd450864ea9
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-04 23:26:46 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -109,9 +109,9 @@ else:
             st.table(df_expectativas_s)
             st.caption('Fuente: Formulario de participación en 2do Encuentro MAU (3/12/2022)')
 
-# Análisis FODA
+# Análisis FODA (12/2022)
 
-st.subheader('Análisis FODA')
+st.subheader('Análisis FODA (12/2022)')
 st.markdown('El análisis FODA es una herramienta de investigación participativa que permitió identificar características comunes entre los diferentes \
 espacios que forman el MAU. Para ello se consideraron 4 marcos de análisis: Debilidades, Amenazas, Fortalezas y Oportunidades.')
 
