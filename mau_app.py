5870793680b18f5b5a9fa2bb3cbb7df2aca5a315
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-05 01:17:35 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -69,7 +69,7 @@ def dms2dd(s):
 df_geo = df_bbdd[['Latitud','Longitud']].dropna(how = 'all')
 df_geo = df_geo.replace(['No info'], '').dropna(how = 'all')
 df_geo = df_geo.replace(['No Info'], '').dropna(how = 'all')
-st.write(df_geo)
+$st.write(df_geo)
 #df_geo['Latitude']  = df_geo['Latitude'].apply(dms2dd)
 #df_geo['Longitude'] = df_geo['Longitude'].apply(dms2dd)
 
