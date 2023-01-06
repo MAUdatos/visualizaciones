15f429a080693fbbf5f9ea0f3e28755ef0aae389
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-06 04:04:44 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import re
+import time
 
 #__________________________________________________________________________________________________________________________________________________________________
 # Dashboard structure
