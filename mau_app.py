91877e7806435a0c1f6fde7a7eead79a25a29c4e
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-05 23:34:43 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -49,12 +49,13 @@ with col1:
 ### Mau en números
 
 # Key Variables
-total_members     = df_bbdd['Organización_Huerta_Colectivo'].nunique()
-total_individuals = df_bbdd['Nombre_representante'].nunique()
-total_localidad   = df_bbdd['Localidad'].nunique()
-
 with col2:
     st.subheader('MAU en números')
+
+    total_members     = df_bbdd['Organización_Huerta_Colectivo'].nunique()
+    total_individuals = df_bbdd['Nombre_representante'].nunique()
+    total_localidad   = df_bbdd['Localidad'].nunique()
+            
     left_column, middle_column, right_column, empty_column = st.columns(4)
     with left_column:
         st.metric("Nº Organizaciones, Huertas y/o Comunidades",total_members)
