42327af94be185f9b71fb2321abe31ae9da7786a
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-06 00:58:12 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -25,10 +25,12 @@ df_expectativas = pd.read_csv('expectativas2doencuentro2022.csv',sep=';').dropna
 
 # General Information for the main page
 st.caption('Sistematización y Mapeo. Prototipo Web App  1.0')
-col1, col2, col3, col4 = st.columns((2,3,0.5,2))
+col1, col2, col3, col4 = st.columns((2,0.5,3,0.5,2))
+col1.image("logo_mau.png", width=200)
+col2.text("  ")
 col2.text("  ")
 col2.subheader("Red de cooperación mutua que fomenta, reivindica y defiende el oficio de la agroecología en pro de la soberanía alimentaria")
-col4.image("logo_mau.png", width=200)
+
 
 ### Objetivos
 # Tabs to organize information
