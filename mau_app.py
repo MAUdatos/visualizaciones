0a2dfae5cf7d1ea9ee5ce42b34645ba729463da1
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-06 04:04:11 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -38,6 +38,33 @@ col3.text("  ")
 col3.subheader("Red de cooperación mutua que fomenta, reivindica y defiende el oficio de la agroecología en pro de la soberanía alimentaria")
 st.markdown("  ")
 
+
+
+progress_bar = st.progress(0)
+status_text = st.empty()
+chart = st.line_chart(np.random.randn(10, 2))
+
+for i in range(100):
+    # Update progress bar.
+    progress_bar.progress(i + 1)
+
+    new_rows = np.random.randn(10, 2)
+
+    # Update status text.
+    status_text.text(
+        'The latest random number is: %s' % new_rows[-1, 1])
+
+    # Append data to the chart.
+    chart.add_rows(new_rows)
+
+    # Pretend we're doing some computation that takes time.
+    time.sleep(0.1)
+
+status_text.text('Done!')
+st.balloons()
+
+
+
 #__________________________________________________________________________________________________________________________________________________________________
 st.header('🍃 Información General MAU') 
 #__________________________________________________________________________________________________________________________________________________________________
