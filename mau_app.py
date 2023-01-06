d353f315f6039985e45ad965cb4b6b87c8b09b2e
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-06 14:58:21 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -65,12 +65,13 @@ total_inst        = df_bbdd['Link redes sociales'].nunique()
           
 col1, col2, col3, col4,col5 = st.columns((1.5,1,1,1,3))   #https://blog.streamlit.io/introducing-new-layout-options-for-streamlit/
 
+st.caption("Fuente: Formularios de participación en 1er y 2do Encuentro MAU 2022")
+
 col1.metric("Nº Organizaciones, Huertas y/o Comunidades",total_members)
 col2.metric("Nº Personas representantes",total_individuals)
 col3.metric("Nº Territorios identificados",total_localidad)
 col4.metric("Nº Redes sociales",total_inst)
 
-st.caption("Fuente: Formularios de participación en 1er y 2do Encuentro MAU 2022")
 st.text("")
 
 #https://stackoverflow.com/questions/33997361 
