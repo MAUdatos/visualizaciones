de0c2ab1977217d407be0ffa85c03e4c61dbd79a
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-05 23:05:49 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -36,10 +36,10 @@ st.header('🍃 Información General MAU')
 col_x, col_y = st.columns(2)
 
 col_x.subheader('Objetivos')
-tab1, tab2 = col_x.tabs(["Objetivo General", "Objetivos Específicos"])
-with tab1:
+col_x.tab1, col_x.tab2 = col_x.tabs(["Objetivo General", "Objetivos Específicos"])
+with col_x.tab1:
    col_x.markdown("""- Desarrollar una red de cooperación mutua que fomente, reivindique y defienda el oficio de la agroecología en pro de la soberanía alimentaria""") #(MAU 3/12/2022)
-with tab2:
+with col_x.tab2:
    col_x.markdown("- Generar redes de apoyo para potenciar el intercambio de saberes, experiencias y recursos entre organizaciones y territorios urbanos, \
                 periurbanos y rurales\n- Recuperar y regenerar los espacios para el aumento de la biodiversidad y el cultivo de alimentos \
                 saludables\n- Generar estrategias metodológicas para compartir saberes y experiencias en torno a la agroecología urbana, periurbana y rural")
