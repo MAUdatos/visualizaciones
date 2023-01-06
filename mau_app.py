05ba49fbf345be2b37c35e7531d836d4ef2c9397
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-06 03:23:52 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -78,14 +78,13 @@ def dms2dd(s):
     return dd
 
 df_bbdd.rename(columns = {'Latitud': 'lat', 'Longitud':'lon',},  inplace = True)
-df = df_bbdd[['lat','lon']] #cols must be named lat and lon for map to work - see lib documentation
+df = df_bbdd[['Localidad', 'Region','lat','lon']].drop_duplicates()
 df = df[df['lat'] != 'No info']
 df = df[df['lon'] != 'No Info']
 df['lat'] = df['lat'].apply(dms2dd)
 df['lon'] = df['lon'].apply(dms2dd)
-df = df[~(df['lat'] == df['lon'])] #excluding wrong values in the .csv (latitude and longitude same values)
-
 df_geo = pd.DataFrame(df,columns=['lat','lon'])
+df_geo.update_geos(fitbounds="locations")
 st.map(df_geo)
 
 st.caption("Fuente: Formularios de participación en 1er y 2do Encuentro MAU 2022")
