1ec0e1c36c9ea5ce02d57c36518c4f25d067f5e2
duende85 <121720880+duende85@users.noreply.github.com>
2023-01-07 12:02:26 -0300

    Update mau_app.py

--- mau_app.py
+++ mau_app.py
@@ -81,7 +81,7 @@ st.text("")
 #https://stackoverflow.com/questions/33997361 
 #https://stackoverflow.com/questions/50193159/converting-pandas-data-frame-with-degree-minute-second-dms-coordinates-to-deci
 
-col5, col6, col7 = st.columns((6,1,6))
+col5, col6, col7 = st.columns((8,1,10))
 
 #___________________________________________________________________________________________________________________________________________________________
 # TREEAMAP
